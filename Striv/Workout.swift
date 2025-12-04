0d6455aa2fed02ebf6a77cc7218366a203f4603e
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-12-04 09:09:53 +0100

    And pace in workout

--- Striv/Workout.swift
+++ Striv/Workout.swift
@@ -17,16 +17,31 @@ struct Workout: Identifiable {
     var hr: Double?
     var kcal: Double?
     var elevation: Double?
+    var pace: Pace {
+        Pace(pace:  Double(duration.totalSeconds / 60) / ((distance ?? 1) / 1000))
+    }
     
     struct Duration {
         var hours: Int
         var minutes: Int
         var seconds: Int
+        var totalSeconds: Int
         
         init(_ time: Int) {
             self.hours = time / 3600
             self.minutes = (time % 3600) / 60
             self.seconds = time % 60
+            self.totalSeconds = time
+        }
+    }
+    
+    struct Pace {
+        var minutes: Int
+        var seconds: Int
+        
+        init(pace: Double) {
+            self.minutes = Int(pace / 1)
+            self.seconds = Int(pace.truncatingRemainder(dividingBy: 1) * 60)
         }
     }
 }
