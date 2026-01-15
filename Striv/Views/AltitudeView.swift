d4893d82b877dd8e0347fab8930812c4de96db79
ThibaultGiraudon <thibault.giraudon@gmail.com>
2026-01-15 10:10:10 +0100

    Add func to reduce number of altitudes in chart

--- Striv/Views/AltitudeView.swift
+++ Striv/Views/AltitudeView.swift
@@ -19,11 +19,12 @@ struct AltitudeView: View {
     }
     var body: some View {
         Chart {
-            ForEach(altitudes.indices, id: \.self) { index in
+            let downSample = downsampleAltitudes(altitudes: altitudes)
+            ForEach(downSample.indices, id: \.self) { index in
                 AreaMark(
                     x: .value("Distance", index),
                     yStart: .value("Min", min),
-                    yEnd: .value("Altitude", altitudes[index])
+                    yEnd: .value("Altitude", downSample[index])
                 )
             }
         }
@@ -31,6 +32,25 @@ struct AltitudeView: View {
         .chartLegend(.hidden)
         .chartYAxis(.hidden)
         .chartXAxis(.hidden)
+    }
+    
+    func downsampleAltitudes(
+        altitudes: [Double],
+        maxDisplayPoints: Int = 300
+    ) -> [Double] {
+        guard altitudes.count > maxDisplayPoints else {
+            return altitudes
+        }
+
+        let windowSize = Int(ceil(
+            Double(altitudes.count) / Double(maxDisplayPoints)
+        ))
+
+        return stride(from: 0, to: altitudes.count, by: windowSize).map { start in
+            let end = Swift.min(start + windowSize, altitudes.count)
+            let window = altitudes[start..<end]
+            return window.reduce(0, +) / Double(window.count)
+        }
     }
 }
 
