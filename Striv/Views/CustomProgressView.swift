12983b44a4ec05e3fd3ff4d4addaa52d04dab91b
ThibaultGiraudon <thibault.giraudon@gmail.com>
2026-04-02 16:30:20 +0200

    Improving TargetView Add CustomProgressView

---
+++ Striv/Views/CustomProgressView.swift
@@ -0,0 +1,30 @@
+//
+//  CustomProgressView.swift
+//  Striv
+//
+//  Created by Thibault Giraudon on 02/04/2026.
+//
+
+import SwiftUI
+
+struct CustomProgressView: View {
+    var value: Double
+    var total: Double
+    
+    var body: some View {
+        GeometryReader { geo in
+            let width = geo.size.width
+            Capsule()
+                .glassEffect()
+            
+            Capsule()
+                .fill(.teal)
+                .frame(width: value * width / total)
+        }
+        .frame(height: 12)
+    }
+}
+
+#Preview {
+    CustomProgressView(value: 12.4, total: 25.0)
+}
--- Striv/Views/TargetView.swift
+++ Striv/Views/TargetView.swift
@@ -10,19 +10,45 @@ import SwiftUI
 struct TargetView: View {
     @ObservedObject var dashboardVM: DashboardViewModel
     @ObservedObject var targetVM: TargetViewModel
+    
+    var currentWeekDistance: Double {
+        self.dashboardVM.stats.currentWeek.totalDistance
+    }
+    
     var body: some View {
-        HStack(spacing: 20) {
-            CircleIndicatorView(current: dashboardVM.stats.currentWeek.totalDistance, target: Double(targetVM.distanceTarget), size: 40, lineWidth: 7)
-            VStack(alignment:.leading) {
-                Text("\(targetVM.distanceTarget) km par semaine")
-                    .font(.title2.bold())
-                Text("\(dashboardVM.stats.currentWeek.totalDistance.roundedText(to: 1)) km / \(targetVM.distanceTarget)km")
+        VStack(alignment: .leading) {
+            Text("\(currentWeekDistance.roundedText(to: 1)) km")
+                .font(.system(size: 65).bold())
+            
+            Text("Distance totale")
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            HStack(spacing: 5) {
+                CustomProgressView(value: currentWeekDistance, total: Double(targetVM.distanceTarget))
+                Text("\(targetVM.distanceTarget) km")
+                    .font(.title.bold())
+                    .foregroundStyle(.primaryText)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 5)
+                    .background {
+                        Capsule()
+                            .glassEffect()
+                    }
             }
-            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 5)
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(LinearGradient(colors: [.gray, .teal], startPoint: .topLeading, endPoint: .bottomTrailing).opacity(0.2))
         }
     }
 }
 
 #Preview {
     TargetView(dashboardVM: .init(), targetVM: .init())
+        .padding()
+        .background(Color.background)
 }
