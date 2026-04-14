23abb5876a84b7ff83994b05fd6e57ec59c70417
ThibaultGiraudon <thibault.giraudon@gmail.com>
2026-04-14 09:30:11 +0200

    Add RunnerProfileViewModel: - manage RunnerProfile - fetch or create profile - update profile

---
+++ Striv/ViewModels/RunnerProfileViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  RunnerProfileViewModel.swift
+//  Striv
+//
+//  Created by Thibault Giraudon on 10/04/2026.
+//
+
+import Foundation
+import Combine
+import SwiftData
+
+class RunnerProfileViewModel: ObservableObject {
+    private var context: ModelContext?
+    
+    func setContext(context: ModelContext) {
+        self.context = context
+    }
+    
+    func profileExist() -> Bool {
+        guard let context else { return false }
+        
+        let descriptor = FetchDescriptor<RunnerProfile>()
+        do {
+            return try context.fetch(descriptor).first != nil
+        } catch {
+            return false
+        }
+    }
+    
+    func createProfileIfNeeded() {
+        guard let context else { return }
+        if profileExist() {
+            return
+        }
+        let profile = RunnerProfile(goal: .init(type: .distance, distance: .preset(.tenK)))
+        
+        do {
+            context.insert(profile)
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func save(_ goal: Goal, for profile: RunnerProfile?) -> Bool {
+        guard let context, let profile else {
+
+            return false
+        }
+        
+        do {
+            profile.goal = goal
+            
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+        return true
+    }
+}
